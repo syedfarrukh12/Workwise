99ef2031b8d2135d6e8653e838ad416ec6ef6afc
Personal <personal@Devs-MacBook-Pro.local>
2023-03-21 19:29:40 -0400

    added body parser and a temporary post route to create user

---
+++ backend/routes/users/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Get all users
+router.get('/users', function(req, res, next) {
+  res.send('all Users');
+});
+
+//Get single user
+router.get('/users/:id', function(req, res, next) {
+    res.send('Signle user');
+});
+
+//create user
+router.post('/user', function(req, res, next) {
+    const {name, email, password, role} = req.body;
+    console.log(name, email, password, role);
+    res.status(200).send("Got it successfully")
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 require("dotenv").config({ path: "./config.env" });
 require('./DB/connection')
 
 const dashboardRoute = require('./routes/dashboard/dashboard');
 const notificationRoute = require('./routes/notifications/notification');
+const userRoute = require('./routes/users/users')
 
+//Port Constant
 const PORT = 5000;
 
 app.use(cors());
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send('<code>Welcome to Workwise app</code> ');
 })
 
 app.use(dashboardRoute)
 app.use(notificationRoute)
+app.use(userRoute)
 
 app.listen(PORT, (req, res)=>{
     console.log(`Server is running on port ${PORT}`);
