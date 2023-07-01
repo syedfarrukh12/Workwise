04c69cb84ab025363733585c6f3a3fe81d8601fb
Personal <personal@Devs-MacBook-Pro.local>
2023-07-02 00:06:37 +0500

    code cleanup

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
 //Get all users
-router.get('/users', function(req, res, next) {
-  res.send('all Users');
+router.get("/users", function (req, res, next) {
+  res.send("all Users");
 });
 
 //Get single user
-router.get('/users/:id', function(req, res, next) {
-    res.send('Signle user');
+router.get("/users/:id", function (req, res, next) {
+  res.send("Signle user");
 });
 
 //create user
-router.post('/user', function(req, res, next) {
-    const {name, email, password, role} = req.body;
-    console.log(name, email, password, role);
-    res.status(200).send("Got it successfully")
+router.post("/user", function (req, res, next) {
+  const { name, email, password, role } = req.body;
+  console.log(name, email, password, role);
+  res.status(200).send("Got it successfully");
 });
 
 module.exports = router;
