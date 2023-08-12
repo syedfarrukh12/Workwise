9b890f0a754c8e57d2fbbe152350dbb42c5d788e
Farrukh Gilani <syedfarrukh228185@gmail.com>
2023-08-13 01:14:39 +0500

    added drag and drop in list view

--- client/src/Components/Common/CustomAccordion.jsx
+++ client/src/Components/Common/CustomAccordion.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   Accordion,
   AccordionDetails,
@@ -10,7 +11,6 @@ import { TaskStatus, camelCaseToSentenceCase } from "../utils";
 import { animated, useSpring } from "@react-spring/web";
 import TicketCardSkeleton from "../TicketCard/TicketCardSkeleton";
 import AddIcon from "@mui/icons-material/Add";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { API_URL } from "./apiConfig";
@@ -21,6 +21,7 @@ import {
 } from "../../redux/nonPersistant";
 import ExpandCircleDownOutlinedIcon from "@mui/icons-material/ExpandCircleDownOutlined";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function CustomAccordion({ tasks, loading }) {
   const groupedTasks = {};
@@ -85,103 +86,193 @@ function CustomAccordion({ tasks, loading }) {
       });
   };
 
+  const [tasksState, setTasksState] = useState(groupedTasks);
+  useEffect(() => {
+    setTasksState(groupedTasks);
+    //eslint-disable-next-line
+  }, [tasks]);
+
+  const handleDragEnd = (result) => {
+    const { destination, source } = result;
+  
+    if (!destination) {
+      return;
+    }
+  
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+  
+    // Find the source section and destination section
+    const sourceSection = tasksState[source.droppableId];
+    const destinationSection = tasksState[destination.droppableId];
+  
+    // Get the task that was dragged
+    const draggedTask = sourceSection[source.index];
+  
+    // Update the status of the dragged task
+    const updatedDraggedTask = {
+      ...draggedTask,
+      status: destination.droppableId,
+    };
+  
+    // Remove the task from the source list
+    const updatedSourceSection = Array.from(sourceSection);
+    updatedSourceSection.splice(source.index, 1);
+  
+    // Add the task to the destination list
+    let updatedDestinationSection;
+    if (destinationSection) {
+      // Destination section exists, clone it and insert the dragged task
+      updatedDestinationSection = Array.from(destinationSection);
+      updatedDestinationSection.splice(destination.index, 0, updatedDraggedTask);
+    } else {
+      // Destination section is empty, create a new array with the dragged task
+      updatedDestinationSection = [updatedDraggedTask];
+    }
+  
+    // Update the state with the new task positions
+    setTasksState((prevState) => ({
+      ...prevState,
+      [source.droppableId]: updatedSourceSection,
+      [destination.droppableId]: updatedDestinationSection,
+    }));
+  
+    // Make API call to update the task status
+    axios
+      .put(
+        `${API_URL}/tasks/${selectedProject._id}/${updatedDraggedTask._id}`,
+        updatedDraggedTask
+      )
+      .then((res) => {
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  
   return (
     <animated.div style={styles} className="px-3 !pb-3">
-      {Object.values(TaskStatus).map((status) => (
-        <Accordion
-          onMouseEnter={() => setHover({ value: true, status: status })}
-          onMouseLeave={() => {
-            setHover({ value: false, status: "" });
-            setShowAddTicket({ value: false, status: "" });
-          }}
-          style={{
-            backgroundColor: theme === "dark" ? "#27374D" : "#eaf2f8",
-            border: "none",
-            boxShadow: "none",
-            borderRadius: "10px",
-            marginTop: "5px",
-          }}
-          key={status}
-          defaultExpanded
-          TransitionProps={{ unmountOnExit: true }}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandCircleDownOutlinedIcon />}
-            aria-controls={`${status}-content`}
-            id={`${status}-header`}
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {Object.values(TaskStatus).map((status) => (
+          <Accordion
+            onMouseEnter={() => setHover({ value: true, status: status })}
+            onMouseLeave={() => {
+              setHover({ value: false, status: "" });
+              setShowAddTicket({ value: false, status: "" });
+            }}
+            style={{
+              backgroundColor: theme === "dark" ? "#27374D" : "#eaf2f8",
+              border: "none",
+              boxShadow: "none",
+              borderRadius: "10px",
+              marginTop: "5px",
+            }}
+            key={status}
+            defaultExpanded
+            TransitionProps={{ unmountOnExit: true }}
           >
-            <div className="flex items-center space-x-3">
-              <Typography>{camelCaseToSentenceCase(status)}</Typography>
-              <div
-                className={`rounded-full px-2 shadow-md ${
-                  theme === "dark" ? "bg-[#27374D]" : "bg-[#DDE6ED]"
-                }`}
-              >
-                {groupedTasks[status]?.length}
-              </div>
-              {hover.value && hover.status === status && (
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setShowAddTicket({ value: true, status: status });
-                  }}
-                  className="p-2 ml-5 rounded-md hover:bg-[#a4b7c6] hover:text-[#000] w-5 h-5 flex items-center justify-center cursor-pointer"
+            <AccordionSummary
+              expandIcon={<ExpandCircleDownOutlinedIcon />}
+              aria-controls={`${status}-content`}
+              id={`${status}-header`}
+            >
+              <div className="flex items-center space-x-3">
+                <Typography>{camelCaseToSentenceCase(status)}</Typography>
+                <div
+                  className={`rounded-full px-2 shadow-md ${
+                    theme === "dark" ? "bg-[#27374D]" : "bg-[#DDE6ED]"
+                  }`}
                 >
-                  <AddIcon className="!w-5 !h-5" />
-                </button>
-              )}
-            </div>
-          </AccordionSummary>
-          <AccordionDetails>
-            {loading ? (
-              <TicketCardSkeleton />
-            ) : (
-              <>
-                {showAddTicket.value && showAddTicket.status === status && (
-                  <div className="mt-2 flex items-center justify-between">
-                    <div className="flex items-center space-x-1 w-full lg:w-[30%] ">
-                      <TextField
-                        size="small"
-                        onChange={(e) => {
-                          setTitle(e.target.value);
-                        }}
-                        type="text"
-                        className="w-full p-1 border rounded-md bg-transparent focus:outline-none"
-                        placeholder="Ticket Title"
-                        variant="outlined"
-                      />
-                      <button
-                        onClick={() => handleSubmit(status)}
-                        className="p-2 rounded-md hover:bg-[#a4b7c6] hover:text-[#000] w-5 h-5 flex items-center justify-center cursor-pointer"
-                      >
-                        <AddIcon className="!w-5 !h-5" />
-                      </button>
-                    </div>
-                    <div
-                      onClick={() =>
-                        setShowAddTicket({ value: false, status: "" })
-                      }
-                    >
-                      <CloseOutlinedIcon className="!w-5 !h-5 cursor-pointer" />
-                    </div>
+                  {groupedTasks[status]?.length}
+                </div>
+                {hover.value && hover.status === status && (
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setShowAddTicket({ value: true, status: status });
+                    }}
+                    className="p-2 ml-5 rounded-md hover:bg-[#a4b7c6] hover:text-[#000] w-5 h-5 flex items-center justify-center cursor-pointer"
+                  >
+                    <AddIcon className="!w-5 !h-5" />
+                  </button>
+                )}
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Droppable droppableId={status} key={status}>
+                {(provided) => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {loading ? (
+                      <TicketCardSkeleton />
+                    ) : (
+                      <>
+                        {showAddTicket.value &&
+                          showAddTicket.status === status && (
+                            <div className="mt-2 flex items-center justify-between">
+                              <div className="flex items-center space-x-1 w-full lg:w-[30%] ">
+                                <TextField
+                                  size="small"
+                                  onChange={(e) => {
+                                    setTitle(e.target.value);
+                                  }}
+                                  type="text"
+                                  className="w-full p-1 border rounded-md bg-transparent focus:outline-none"
+                                  placeholder="Ticket Title"
+                                  variant="outlined"
+                                />
+                                <button
+                                  onClick={() => handleSubmit(status)}
+                                  className="p-2 rounded-md hover:bg-[#a4b7c6] hover:text-[#000] w-5 h-5 flex items-center justify-center cursor-pointer"
+                                >
+                                  <AddIcon className="!w-5 !h-5" />
+                                </button>
+                              </div>
+                              <div
+                                onClick={() =>
+                                  setShowAddTicket({ value: false, status: "" })
+                                }
+                              >
+                                <CloseOutlinedIcon className="!w-5 !h-5 cursor-pointer" />
+                              </div>
+                            </div>
+                          )}
+                        {tasksState[status]?.length > 0 ? (
+                          tasksState[status].map((task, index) => (
+                            <Draggable
+                              key={task._id}
+                              draggableId={task._id}
+                              index={index}
+                            >
+                              {(provided) => (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  <TicketCard task={task} />
+                                </div>
+                              )}
+                            </Draggable>
+                          ))
+                        ) : (
+                          <div className="text-sm text-center">
+                            No tasks in this section
+                          </div>
+                        )}
+                      </>
+                    )}
+                    {provided.placeholder}
                   </div>
                 )}
-                {groupedTasks[status]?.length > 0 ? (
-                  groupedTasks[status].map((task) => (
-                    <div key={task._id}>
-                      <TicketCard task={task} />
-                    </div>
-                  ))
-                ) : (
-                  <div className="text-sm text-center">
-                    No tasks in this section
-                  </div>
-                )}
-              </>
-            )}
-          </AccordionDetails>
-        </Accordion>
-      ))}
+              </Droppable>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </DragDropContext>
     </animated.div>
   );
 }
